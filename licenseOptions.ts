cd83b017ee68ea0d0b77e39795b921939b1d39a9
Alex MacArthur <alex@macarthur.me>
2023-07-16 19:35:41 -0500

    finish out main content on home page

---
+++ licenseOptions.ts
@@ -0,0 +1,32 @@
+export default [
+  {
+    slug: "typeit_limited",
+    friendlySlug: "limited",
+    name: "TypeIt - Limited License",
+    htmlTitle: "<strong class='font-bold'>Limited</strong> Commercial License",
+    simpleTitle: "Limited License",
+    price: 900, // must be in cents!
+    description:
+      "Use TypeIt on a single commercial project. Includes lifetime updates.",
+    permissionDescription:
+      "You are now eligible to use TypeIt for a single commercial project.",
+    licenseLink: "https://typeitjs.com/licenses/download/limited-commercial",
+    usageScope: "a single commercial project",
+    priceId: process.env.STRIPE_LIMITED_LICENSE_PRICE_ID,
+  },
+  {
+    slug: "typeit_unlimited",
+    friendlySlug: "unlimited",
+    name: "TypeIt - Unlimited License",
+    htmlTitle: "<strong class='font-bold'>Unlimited</strong> Commercial License",
+    simpleTitle: "Unlimited License",
+    price: 2900, // must be in cents!
+    description:
+      "Unlimited use on any commercial project. Includes lifetime updates.",
+    permissionDescription:
+      "You are now eligible to use TypeIt commercially for an unlimited number of projects.",
+    usageScope: "any number of commercial projects",
+    licenseLink: "https://typeitjs.com/licenses/download/unlimited-commercial",
+    priceId: process.env.STRIPE_UNLIMITED_LICENSE_PRICE_ID,
+  },
+];
